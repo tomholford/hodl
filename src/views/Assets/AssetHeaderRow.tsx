b8e691b769101fb5ab14baaab1d036e1e4d2698e
tomholford <tomholford@users.noreply.github.com>
2022-01-12 12:51:53 -0800

    style the header row too

--- src/views/Assets/AssetHeaderRow.tsx
+++ src/views/Assets/AssetHeaderRow.tsx
@@ -4,15 +4,15 @@ export const AssetHeaderRow = () => {
   return (
     <>
       <div className="asset-header-row">
-        <div className="asset-balance">
+        <div className="asset-balance header-label">
           balance
         </div>
-        <div className="asset-value">current value</div>
-        <div className="asset-pnl">P / L</div>
-        <div className="asset-cost-basis">cost basis</div>
-        <div className="asset-acquired-date">date acquired</div>
-        <div className="asset-note">note</div>
-        <div className="asset-action">actions</div>
+        <div className="asset-value header-label">current value</div>
+        <div className="asset-pnl header-label">P / L</div>
+        <div className="asset-cost-basis header-label">cost basis</div>
+        <div className="asset-acquired-date header-label">date acquired</div>
+        <div className="asset-note header-label">note</div>
+        <div className="asset-action header-label">actions</div>
       </div>
     </>
   )
